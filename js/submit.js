0c4ec1194382670b644f862070295bfbceb845cf
Natalieabbie <abionatline@gmail.com>
2021-05-20 08:28:44 +0300

    modify the submit button

---
+++ js/submit.js
@@ -0,0 +1,13 @@
+
+        function testVariable() {
+            debugger
+            var strText = document.getElementById("full_name").value;
+            var strText1 = document.getElementById("review").value;
+            document.getElementById("p1").innerHTML = strText;
+            document.getElementById("p2").innerHTML = strText1;
+
+        }
+   
+
+
+
