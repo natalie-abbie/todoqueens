74c70538b90de86376232de45c8afa294eab217f
Natalieabbie <abionatline@gmail.com>
2021-05-20 16:02:48 +0300

    add changes

--- js/submit.js
+++ js/submit.js
@@ -3,10 +3,10 @@
 function submitcomment() {
     debugger
     
-var strText = document.getElementById("full_name").value;
-var strText1 = document.getElementById("review").value;     
-document.getElementById("p1").innerHTML = strText;
-document.getElementById("p2").innerHTML = strText1;
+var full_name = document.getElementById("full_name").value;
+var review = document.getElementById("review").value;     
+document.getElementById("p1").innerHTML = full_name;
+document.getElementById("p2").innerHTML = review;
 document.getElementById("review-form").reset();
 alert("Thank you!, you review was submitted");
 
