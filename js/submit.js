8b4e5b5cf3be6be2db7d8484bda5c3b8d90d5e9e
Natalieabbie <abionatline@gmail.com>
2021-05-20 15:43:35 +0300

    modify the changes

--- js/submit.js
+++ js/submit.js
@@ -1,32 +1,29 @@
-// validation .
-function validation(){
-    var full_name = document.getElementById("full_name").value;
-    var review = document.getElementById("review").value;
-    if( full_name ==='' || review ===''){
-    alert("Please fill all fields...!!!!!!");
-    return false;
-    }else{
-    return true;
-    }
-    }
-        
-        function submitcomment() {
-            debugger
-            // var x = document.getElementsByTagName("form");
-            // x[0].submit();// Form submission
-            // alert("Form Submitted Successfully......");
-            var strText = document.getElementById("full_name").value;
-            var strText1 = document.getElementById("review").value;        
-            document.getElementById("p1").innerHTML = strText;
-            document.getElementById("p2").innerHTML = strText1;
-            document.getElementById("review-form").reset();
-            
 
-        }
+
+function submitcomment() {
+    debugger
+    
+var strText = document.getElementById("full_name").value;
+var strText1 = document.getElementById("review").value;     
+document.getElementById("p1").innerHTML = strText;
+document.getElementById("p2").innerHTML = strText1;
+document.getElementById("review-form").reset();
+alert("Thank you!, you review was submitted");
+
+}
+
+
    
 
-      
-
-
-
-
+//         // validation .
+// function validation(){
+//     var full_name = document.getElementById("full_name").value;
+//     var review = document.getElementById("review").value;
+//     if( full_name ==='' || review ===''){
+//     alert("Please fill all fields...!!!!!!");
+//     return false;
+//     }else{
+//     return true;
+//     }
+// }
+    
